a6f30abe0563b3cd1f6bfcd56a7d2855c39093e5
dylanmolnarMAXAR <67292325+dylanmolnarMAXAR@users.noreply.github.com>
2021-10-29 15:42:04 -0600

    a few more tweaks

--- mosaic_4band_3-2-1.sh
+++ mosaic_4band_3-2-1.sh
@@ -5,10 +5,16 @@ gdalbuildvrt newvrt.vrt *.tif
 echo 'vrt complete, now rendering as rgb and compressing'
 
 #convert the vrt to show the visible bands 3,2,1 for 4-band ps
-#use deflate compression
-gdal_translate -dstnodata 0 -b 3 -b 2 -b 1 newvrt.vrt rgb-mosaic.tif -co COMPRESS=DEFLATE -co PHOTOMETRIC=rgb -co BIGTIFF=YES -co ALPHA=NO
+gdal_translate newvrt.vrt 3-2-1_vrt.vrt -b 3 -b 2 -b 1
 
-#remove vrt
-rm newvrt.vrt
+#scale to 8bit
+#I just put 0 2500, seems to be close to most of the tifs I have used. use "gdalinfo -mm" to find exact
+gdal_translate -ot Byte -scale 0 2500 0 255 -exponent 0.4 3-2-1_vrt.vrt mosaic-rgb.vrt
+
+#use deflate compression and convert to tif
+gdal_translate mosaic-rgb.vrt mosaic-rgb.tif -co COMPRESS=DEFLATE -co BIGTIFF=YES
+
+#remove all vrt files
+del *.vrt
 
 echo 'complete'
